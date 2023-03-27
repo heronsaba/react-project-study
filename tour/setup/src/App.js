910ffe7591b516d8ab6467192bc41db6977f7dc6
Heron Saba <heronsaba.hs@gmail.com>
2023-03-26 23:39:54 -0300

    tour project add

---
+++ tour/setup/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tour from './Tour';
+import Tours from './Tours'
+const url = 'https://course-api.com/react-tours-project'
+
+
+function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+      setTours(data);
+    }
+    catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return <main>
+      <Loading />
+    </main>
+  }
+  return <main>
+    <Tours tours={tours}/>
+  </main>
+}
+
+export default App
