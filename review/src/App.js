7963e3add1b5fb9af6737f114b4c63cdd1a63787
Heron Saba <heronsaba.hs@gmail.com>
2023-03-28 22:52:49 -0300

    review

--- review/src/App.js
+++ review/src/App.js
@@ -3,14 +3,15 @@ import Review from './Review';
 
 function App() {
   return (
-  <main>
-    <section className='container'>
-      <div className='title'>
-        <h2>our reviews</h2>
-        <div className='underline'></div>
-      </div>
-    </section>
-  </main>
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
+    </main>
   )
 }
 
